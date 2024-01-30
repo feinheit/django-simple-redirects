a45a7d98362a726652d7d7e16ad12ce19fa4696f
Matthias Kestenholz <mk@feinheit.ch>
2024-01-30 16:03:19 +0100

    Add search fields

--- simple_redirects/admin.py
+++ simple_redirects/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from simple_redirects.models import Redirect
 
 
-admin.site.register(Redirect, list_display=("old_path", "new_path"))
+admin.site.register(
+    Redirect,
+    list_display=("old_path", "new_path"),
+    search_fields=("old_path", "new_path"),
+)
